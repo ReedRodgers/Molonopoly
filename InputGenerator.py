47d82161b27ffef771e14d6c8fd2199b09098574
Reed Rodgers <reed.rodgers@gmail.com>
2020-03-25 16:08:13 -0400

    Stub out network, slight refactor of InputGenerator

--- InputGenerator.py
+++ InputGenerator.py
@@ -4,23 +4,26 @@ Step 1: evaluate the value of different combinations of properties
 '''
 
 class Property:
-    self.cost
     def __init__(self, cost, color):
         self.cost = cost
         self.color = color
 
 class Board:
-    self.properties
     def __init__(self, properties):
         self.properties = properties
 
-    def find_all_combos(self):
-        return [[]]
-
-class player:
-    self.cash
-    self.properties
+class Player:
     def __init__(self, cash, properties):
         self.cash = cash
         self.properties = properties
 
+
+'''Given a board, returns all the possible combinations of properties'''
+def find_all_combos(board: Board):
+    return [[]]
+
+'''Finds all viable combinations of cash and property for a given player on a given board'''
+def generate_training_input(board: Board, player: Player):
+    return [{'cash': 0,
+             'properties': [Property(0, 'blue')]
+             }]
---
+++ network.py
@@ -0,0 +1,24 @@
+"""Given property cash combos from InputGenerator, return array datatype for learning"""
+def read_board(property_combos):
+    return []
+
+"""return heuristic value of property cash combination"""
+def evaluate_heuristic(input, output):
+    return 0
+
+"""
+return trainging values for property combinations by feeding them to the heuristic
+Accepts a list of cash value and property dictionaries
+"""
+def y_values(x_values):
+    return []
+
+"""
+Entry point method to be called from main.
+Accepts property combinations, processes them, and trains the model
+"""
+def train(property_combos):
+
+
+def predict(property_combo):
+    return 0
