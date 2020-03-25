eae7f5e5d2628762a1ae385eae7d71feed03c617
Reed Rodgers <reed.rodgers@gmail.com>
2020-03-25 15:46:36 -0400

    Stub out InputGenerator

---
+++ InputGenerator.py
@@ -0,0 +1,26 @@
+'''
+This file is responsible for generating input for the NN.
+Step 1: evaluate the value of different combinations of properties
+'''
+
+class Property:
+    self.cost
+    def __init__(self, cost, color):
+        self.cost = cost
+        self.color = color
+
+class Board:
+    self.properties
+    def __init__(self, properties):
+        self.properties = properties
+
+    def find_all_combos(self):
+        return [[]]
+
+class player:
+    self.cash
+    self.properties
+    def __init__(self, cash, properties):
+        self.cash = cash
+        self.properties = properties
+
