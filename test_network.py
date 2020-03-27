4d831c1a2e2f2339cffa299905018334e5257b53
Reed Rodgers <reed.rodgers@gmail.com>
2020-03-27 12:35:53 -0400

    Fixed read_board test, verified read_board

--- test_network.py
+++ test_network.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from InputGenerator import Board, Property
 from network import read_board
+import numpy as np
 
 pb1 = Property(100, 'b')
 pb2 = Property(100, 'b')
@@ -32,18 +33,20 @@ class Test(TestCase):
         combo3 = {'cash': 100, 'properties': props3}
         combo4 = {'cash': 100, 'properties': props4}
         combo4_1 = {'cash': 100, 'properties': props4_1}
-        output1 = [100, 1, 1]
-        output2 = [100, 1]
-        output3 = [100, 1, 1, 1, 0, 1, 0]
-        output4 = [100,
+        output1 = np.array([100, 1, 1])
+        output2 = np.array([100, 1])
+        output3 = np.array([100, 1, 1, 1, 0, 1, 0])
+        output4 = np.array([100,
                    1, 1, 1,
                    1, 1, 1,
-                   1, 1, 1]
-        output4_1 = [100,
+                   1, 1, 1])
+        output4_1 = np.array([100,
                      1, 1, 0,
                      1, 1, 0,
-                     0, 0, 0]
-        self.assertEqual([output1], read_board([combo1], board1))
-        self.assertEqual([output2], read_board([combo2], board2))
-        self.assertEqual([output3], read_board([combo3], board3))
-        self.assertEqual([output4, output4_1], read_board([combo4, combo4_1], board4))
+                     0, 0, 0])
+        np.testing.assert_array_equal(output1, read_board([combo1], board1)[0])
+        np.testing.assert_array_equal(output2, read_board([combo2], board2)[0])
+        np.testing.assert_array_equal(output3, read_board([combo3], board3)[0])
+
+        for output, expected in zip(read_board([combo4, combo4_1], board4), [output4, output4_1]):
+            np.testing.assert_array_equal(output, expected)
