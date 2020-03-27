e1e078a379224b9f5b062dbed4fb0806d4952696
Reed Rodgers <reed.rodgers@gmail.com>
2020-03-27 12:22:18 -0400

    read_board (UNTESTED)

--- network.py
+++ network.py
@@ -1,15 +1,28 @@
 from InputGenerator import Board, generate_training_input, find_all_combos, Player
 import numpy as np
+from collections import Counter
 
 def read_board(combos, board: Board):
     """Given property cash combos from InputGenerator, return array datatype for learning"""
-    colors = board.get_colors()
-    breadth = len(colors)
-    depth = colors.most_common()[0][1]
+    color_counts = board.get_colors()
+    breadth = len(color_counts)
+    depth = color_counts.most_common()[0][1]
+    color_list = board.get_color_list()
+    color_dict = {color: i for i, color in enumerate(color_list)}
     data = []
     for combo in combos:
-        mtx = np.zeros([depth, breadth], dtype=int)
-    return []
+        mtx = np.zeros([breadth, depth], dtype=int)
+        color_count = Counter(color_list)
+        color_count.subtract(color_list)
+        for prop in combo['properties']:
+            color = prop.color
+            mtx[color_dict[color], color_count[color]] = 1
+            color_count.update([color])
+        cash = np.array([combo['cash']])
+        ls = mtx.flatten()
+        data.append(np.concatenate((cash, ls)))
+
+    return data
 
 def evaluate_heuristic(input, output):
     """return heuristic value of property cash combination"""
