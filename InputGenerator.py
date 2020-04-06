f9b68b849e5561c1573a5db14251521156ff9474
Ishan <mishra.ishan@gmail.com>
2020-04-06 17:51:51 -0400

    Built all the mechanics for Stage 2

--- InputGenerator.py
+++ InputGenerator.py
@@ -2,7 +2,7 @@
 This file is responsible for generating input for the NN.
 Step 1: evaluate the value of different combinations of properties
 """
-
+from random import randint
 
 class Square:
     def __init__(self):
@@ -17,66 +17,57 @@ class Square:
 
 
 class Property(Square):
-    def __init__(self, cost, colour, index):
+    def __init__(self, cost, colour, rent, name):
         self.cost = cost
         self.colour = colour
-        self.index = index
-        self.under_management = False
+        self.rent = rent
+        self.name = name
+        self.owner = False
         self.players_present = []
 
     def __str__(self):
-        return f'{self.colour}{self.index}'
+        return f'{self.name}'
 
     def __repr__(self):
-        return f'{self.colour}{self.index}'
+        return f'{self.name}'
 
 
 class Board:
-    def __init__(self, properties,  colours):
-        self.colours = colours
-        self.properties = properties
-
-        self.full_board = []
-        for idx, prop in enumerate(properties):
-            if idx % (len(properties) / len(colours)) == 0:
-                self.full_board.append(Square())
-            self.full_board.append(prop)
-        # assert self.full_board.count(Square()) == 4, "Board is not a square"
+    def __init__(self, properties):
+        self.full_board = properties  # List with objects of type: Property
+        self.property_count = len(properties)  # Store this variable so that it's not evaluated multiple times.
 
     def debug(self):
         """Print out every single square on the board, if it's owned, if there's a player on that square"""
         for square in self.full_board:
-            if type(square) == Property:
-                print(f'{square}; owned={square.under_management}; players_present={square.players_present}')
-            else:
-                print(f'{square}; players_present={square.players_present}')
+            print(f'{square}; owned={square.owned_by}; players_present={square.players_present}')
 
-    def roll(self):
-        pass
+    def roll(self, player):
+        current_position = player.position
+        dice_roll = randint(0, 7)
+
+        # Use modulus to ensure circular behaviour
+        new_position = (current_position + dice_roll) % self.property_count
+
+        # Remove player from old position
+        self.full_board[current_position].players_present.remove(player)
+
+        # Change player's position
+        player.position = new_position
+        self.full_board[new_position].players_present.append(player)
+
+        return new_position
 
 
 class Player:
-    def __init__(self, cash, starting_position):
+    def __init__(self, cash):
         self.cash = cash
         self.properties = []   # Perhaps this should be a dictionary, with 4 keys corresponding to the colours
-        self.position = starting_position
+        self.position = 0
 
     def purchase(self, some_property: Property):
         self.cash -= some_property.cost
         self.properties.append(some_property)
-        some_property.under_management = True
+        some_property.owner = self
 
 
-def find_all_combos(board: Board, player: Player):
-    """Given a board, returns all the possible combinations of properties"""
-    unclaimed_properties = [i for i in board.properties if i.under_management is False]
-    owned_properties = player.properties
-    purchases_possible = player.cash/100
-    return [[]]
-
-
-def generate_training_input(combos, player: Player):
-    """Finds all viable combinations of cash and property for a given player on a given board"""
-    return [{'cash': 0,
-             'properties': [Property(0, 'blue')]
-             }]
--- main.py
+++ main.py
@@ -1,34 +1,79 @@
 import InputGenerator
 import network
+from queue import Queue as queue
+
+def read_properties():
+    """Generator function to read from csv"""
+    with open("property_list.csv", "r", encoding="utf-8-sig") as f:
+        yield from f
 
 
-def initialize_stage1_game(property_colours=['red', 'blue', 'green', 'purple'],
-                           property_per_colour=5, cost_per_property=100):
+def import_properties():
+    property_list = []
+    data = read_properties()
+    for entry in data:
+        name, price, rent, col = entry.strip().split(",")
+        property_list.append(InputGenerator.Property(name=name, cost=price, colour=col, rent=rent))
+    return property_list
+
+
+def initialize_stage2_game(player_count=1):
     """ Initialize the entire board and first player, as defined in Stage 1"""
 
     # Generate property list
-    property_list = [InputGenerator.Property(cost_per_property, colour, i)
-                     for colour in property_colours for i in range(property_per_colour)]
+    property_list = import_properties()
 
-    board = InputGenerator.Board(property_list, property_colours)
-    player1 = InputGenerator.Player(property_per_colour * cost_per_property, board.full_board[0])
-    board.full_board[0].players_present.append('player1')
-    return property_list, board, player1
+    # Create board using the properties provided
+    board = InputGenerator.Board(property_list)
+
+    # Spawn players with money, on the first tile of the board.
+    players = queue()
+    for _ in range(player_count):
+        new_player = InputGenerator.Player(cash=1500)
+        players.put(new_player)
+        board.full_board[0].players_present.append(new_player)
+    return property_list, board, players
+
+
+def stage2_transactions(player: InputGenerator.Player, property: InputGenerator.Property):
+    """ In stage 2, the player can either buy a property, or will have to pay rent for it."""
+
+    if not property.owner:
+        pass
+        # Make purchasing decision(s)
+    return None
+
+
+def run_game(game_board:InputGenerator.Board, players: queue, rounds_per_game=25):
+    turns = 0
+    while turns < rounds_per_game * players.qsize():
+        # Who's turn is it?
+        current_player = players.get()
+
+        # Roll the dice, and move the player accordingly
+        current_property = game_board.full_board[game_board.roll(current_player)]
+
+        # Make transaction decisions
+        stage2_transactions(player=current_player, property=current_property)
+
+        # Add the player to the end of the queue
+        players.put(current_player)
+
+        # Mark turn as done
+        turns += 1
 
 
 if __name__ == '__main__':
 
     # Initialize board
-    properties, new_board, main_player = initialize_stage1_game()
+    properties, new_board, player_list = initialize_stage2_game()
 
     # Ensure board is setup properly
     new_board.debug()
 
-    # Get all combinations
-    all_combos = InputGenerator.find_all_combos(new_board)
+    # Run game
+    run_game(new_board, players=player_list)
 
-    # Discern viable combinations
-    viable_combos = InputGenerator.generate_training_input(all_combos, main_player)
+    # Determine winner(s)
 
-    # Train network
-    network.train(viable_combos)
+
