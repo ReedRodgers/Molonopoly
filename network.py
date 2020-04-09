0c1e5d7eeeb27bb55b41a2ac12afbcf889f4531f
Reed Rodgers <reed.rodgers@gmail.com>
2020-04-09 17:52:24 -0400

    Stub out network class

--- network.py
+++ network.py
@@ -1,19 +1,33 @@
+class network:
+
+    def __init__(self, net, name):
+        self.net = net
+        self.previous = None
+        self.name = name
+
+    def load_from_weights(self):
+        """Read in weight csv file"""
+
+    def predict(self, players, me):
+        """Given a list of players, and an identifier of which player is represented by the net,
+        return the expected value of the board"""
+
+    def learn(self, prev, curr):
+        """applies learning by comparing previous estimate to current estimate"""
+
+    def save(self):
+        """saves weights as .csv"""
+
+
+
+
+
+
+
+
+
 def read_board(property_combos):
     """Given property cash combos from InputGenerator, return array datatype for learning"""
-    return []
-
-
-def evaluate_heuristic(property_ownership, output):
-    """return heuristic value of property cash combination"""
-
-    return 0
-
-
-def apply_heuristic(x_values):
-    """
-    return training values for property combinations by feeding them to the heuristic
-    Accepts a list of cash value and property dictionaries
-    """
     return []
 
 
